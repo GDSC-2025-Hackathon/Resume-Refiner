06b9328e78ef24663dd84e313f0dcec07f875ca4
Paul Elmer <paulelmer112@hotmail.com>
2025-05-03 15:08:01 -0400

    commiting react stuff

---
+++ resume-reviser/src/App.jsx
@@ -0,0 +1,55 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  const [text, setText] = useState('');
+
+  const handleChange = (event) => {
+    setText(event.target.value);
+  };
+
+  return (
+    <>
+      <div>
+        {/* 
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a> */}
+      </div>
+      <h1>Resume Reviser</h1>
+      <div className="card">
+        <button onClick>
+          Choose file
+        </button>
+
+        <button onClick>
+          Upload file
+        </button>
+        
+      </div>
+
+      <h2>Enter Job Description</h2>
+      <div>
+      
+      <input
+        type="text"
+        value={text}
+        onChange={handleChange}
+        placeholder="Type something..."
+      />
+      <p>You typed: {text}</p>
+    </div>
+    </>
+  )
+}
+
+
+
+export default App
